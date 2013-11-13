998240845f7f6bc66773fa771a35e664e4f4dad6
Jonathan Raí Samines Solares <jn.samines@gmail.com>
2013-11-12 21:41:52 -0600

    Se agrega script principal SignalJS y test.

---
+++ object.signal.js
@@ -0,0 +1,62 @@
+
+// Object.Signal - Implementación de publicador/suscriptor sobre extensión en objetos.
+// Permite detectar cambios en objetos
+
+
+( function( signal ){
+
+	// Se verifica que signal haya sido incluido
+	if ( window.signal == undefined ) {
+		return;
+	} 
+
+
+	// Componente principal de opciones
+	var component = {};
+
+	// Eventos soportados
+	component.events = {
+		onChange : "change"
+	};
+
+
+	window.Entity = function(){};
+
+	// Se extiende la clase Entity, para permitir observar sus propiedades
+	// se agregan metodos para establecer y obtener cada una de las propiedades
+	Entity.prototype.set = function( properties , values ){
+
+		// se verifica el valor de values, si values === undefined
+		// entonces quiere decir, que se usó el setter por objeto,
+		// por lo que se busca a través de cada propiedad indicada.
+		if ( values === undefined ){
+
+			for( var prop in properties ){
+				// Se establece el nuevo valor para una propiedad,
+				// si esta ha sido predefinida originalmente.
+				if( this[prop] !== undefined ){
+					// emitir evento de cambio de valor
+					signal.emit( component.events.onChange + ":" +  prop, { old : this[prop], "new" : properties[prop] });
+					this[prop] = properties[prop];
+				}
+			}
+		// de lo contrario se asume, que se usa el setter simple, por lo que solo se busca
+		// la propiedad en concreto indicada
+		}else{
+
+			if ( this[properties] !== undefined ){
+				// emitir evento de campo de valor
+				signal.emit( component.events.onChange + ":" +  properties, { old : this[properties], "new" : values });
+				this[properties] = values;
+			}
+		}
+
+		return this;
+	};
+
+
+})( signal );
+
+
+
+
---
+++ signal.js
@@ -0,0 +1,89 @@
+﻿
+
+// SignalJS - Implementación básica para el patrón publisher / suscriber
+// Permite el uso del patrón publicador / suscriptor
+var signal = {};
+
+(function( exports ){
+
+	// Componente principal de la aplicación, que permite manejar todas las funciones
+	// utilitarias y propiedades del sistema.
+	var component = {
+		subscribers_by_topic : [],
+		identifier : 0
+	};
+
+
+	// Función para adición de suscriptores a un tema en específico.
+	// <param name="topic"> Tema al cual se va a realizar la suscripción. </param>
+	// <param name="func"> Función de suscripción. </param>
+	exports.subscribe = function( topic, func ){
+
+		// se agrega el nuevo tema a la lista de suscripciones, si no ha sido agregado.
+		if ( component.subscribers_by_topic[ topic ]  === undefined ){
+			component.subscribers_by_topic[ topic ] = [];
+		}
+
+		// Para crear identificadores únicos por tema de suscripción,
+		// se inicializa el campo "identifier" como la cantidad de suscriptores actuales
+		component.identifier = component.subscribers_by_topic[ topic ].length;
+		component.identifier++;
+
+		// Se agrega la nueva suscripcion, a la lista de suscripcion del tema especificado.
+		// se asigna tanto la función de notificación, como el identificador generado ( para posteriores suscripciones )
+		component.subscribers_by_topic[ topic ].push({ signal : func, identifier : component.identifier })
+
+		// Se devuelve el identificador generado.
+		return component.identifier;
+	};
+
+
+	// Función para cancelar la suscripción a un tema específico.
+	// <param name="topic"> Tema al cual se desea cancelar la suscripción. </param>
+	// <param name="identifier"> Identificador de la suscripción. ( Este es generado al momento de realizar la suscripción al tema ). </param>
+	exports.unsubscribe = function( topic , identifier ){
+		
+		// se obtiene la lista de suscriptores para el tema en específico.
+		var subscribers = component.subscribers_by_topic[ topic ];
+
+		// Si el tema de suscripción no existe, se devuelve falso
+		if ( subscribers === undefined ){
+			return false;
+		}
+
+		// se recorre la lista de suscripcion para el tema, para buscar el identificador proporcionado.
+		for( var suscriptor = 0; suscriptor < subscribers.length ; suscriptor++ ){
+			if ( subscribers[ suscriptor ].identifier === identifier ){
+				subscribers.splice( suscriptor, 1 );
+				return true;
+			}
+		}
+
+		return false;
+	};
+
+	// Función para la publicación de una nueva notificación a un tema específico
+	// <param name="topic"> Tema al cual se realizará la publicación. </param>
+	// <param name="data"> Información pasada a través de la función de suscripción de un suscriptor. </param>
+	exports.emit = function( topic, data ){
+
+		// se obtiene la lista de suscriptores para el tema en específico.
+		var subscribers = component.subscribers_by_topic[ topic ];
+
+		// Se verifica que el tema al cual se realizará la publicación exista.
+		if ( subscribers === undefined ){
+			return;
+		}
+
+		// Se recorren todos los suscriptores encontrados para el tema
+		for( var suscriptor = 0; suscriptor < subscribers.length; suscriptor ++){
+
+			// se notifica a cada suscriptor acerca de la nueva publicación,
+			// pasando la información especificada al suscritor.
+			subscribers[ suscriptor ].signal( data );
+		}
+
+	};
+
+
+})( signal );
